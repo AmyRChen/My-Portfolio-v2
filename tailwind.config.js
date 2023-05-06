be0a086eb94b1b76754e3238d077fbaf0a2b85a9
Ping-Ju, Chen <cpr860632231@gmail.com>
2023-05-06 13:11:00 -0400

    first init

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        white: "#F8F3F0",
+        black: "#231F1C",
+        "dark-grey": "#404040",
+        olive: "#999887",
+        tea: "#C6BBAF",
+        "deep-white": "E3E0D9",
+        "deep-tea": "#B0A69B",
+      },
+      fontFamily: {
+        roboto: ["Roboto Mono", "monospace"],
+        ubuntu: ["Ubuntu", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
